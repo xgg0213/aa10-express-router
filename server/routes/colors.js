0c29c9a7ce10eade02e38064dfc00c2dae3ea9c5
xgg0213 <m.eringan@gmail.com>
2024-07-26 13:50:23 -0700

    bonus phase completed

--- server/routes/colors.js
+++ server/routes/colors.js
@@ -7,9 +7,20 @@ router.get('/', (req, res) => {
     res.json('GET /colors')
 })
 
+router.delete('/:name/css-style/:style', (req, res) => {
+    res.json("DELETE /colors/:name/css-styles/:style")
+})
+
+router.post('/:name/css-style', (req, res) => {
+    res.json("POST /colors/:name/css-styles")
+})
+
 // /colors/:name
 router.get('/:name', (req, res) => {
     res.json('GET /colors/:name')
 })
+
+
+
 
 module.exports = router;
